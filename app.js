7003c0bbc28200315d0b166a61eb5e81d631052a
Mathew Torres <torres.mathew@gmail.com>
2022-09-14 13:03:16 -0700

    solve step 1

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 // STEP 1
 // This is a standard function expression. You may also be familiar with function declarations, which begin with the "function" keyword.
-const theOldWay = function (course) {
-  return `I am currently enrolled in ${course}`;
-};
+// const theOldWay = function (course) {
+//   return `I am currently enrolled in ${course}`;
+// };
+
+const theOldWay = (course) => `I am currently enrolled in ${course}`
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The old way:', theOldWay('Code 301'));
+console.log('The old way:', theOldWay('Code 301'));
 
 
 // STEP 2
